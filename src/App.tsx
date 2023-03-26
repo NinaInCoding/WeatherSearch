5b838cf64bd2778df9ef17a66f3f52630ad666f4
Nina <bbbonnny@gmail.com>
2023-03-26 23:02:40 +0100

    update(*): init project, update modules

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App(): React.ReactElement {
+  // const [count, setCount] = useState(0)
+
+
+  return (
+    <div className="App">
+      <p>test</p>
+    </div>
+  )
+}
+
+export default App
